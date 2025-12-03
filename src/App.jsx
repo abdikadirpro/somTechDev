983e8405b9f566aaa0d40420414956599a524be8
ahmed mohamed <amlaarkish@gmail.com>
2025-12-03 20:23:35 +0300

    header part finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Service from "./pages/Service"
+import Blogs from "./pages/Blogs"
+import Header from "./Components/home/Header"
+import Contacts from "./pages/Contacts"
 
 
 const App = () => {
   return (
-    <div className='flex justify-between  bg-blue-400 my-2 p-10'>
-     <h1 className="text-3xl font-bold text-white uppercase  ">
-  Tailwind 4 is working!
-   </h1>
-   <ul className="flex gap-10 text-white font-bold bg-primary font-serif  sm:w-1/2 md:w-1/3 lg:w-1/4 capitalize text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl hover:*:text-red-600">
-    <li>mondey</li>
-    <li>tuesdey </li>
-    <li>friday</li>
-    <li>friday</li>
-    <li>suterday</li>
-   </ul>
-    </div>
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/service" element={<Service/>}/>
+    <Route path="/blogs" element={<Blogs/>}/>
+    <Route path="/contacts" element={<Contacts/>}/>
+  </Routes>
+  
+  </BrowserRouter>
   )
 }
 
