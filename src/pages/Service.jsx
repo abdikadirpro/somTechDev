ad6059252a48fcc6ef8c8ff447608b122b892123
ahmed mohamed <amlaarkish@gmail.com>
2025-12-13 19:49:45 +0300

    service

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import Web from '../Components/service/Web'
+import Application from '../Components/service/Application'
+import Software from '../Components/service/Software'
+import Maintenance from '../Components/service/Maintenance'
+import Branding from '../Components/service/Branding'
+import UiUx from '../Components/service/UIUX'
 const Service = () => {
   return (
-    <div>Service</div>
+    <div>
+    <Web/>
+    <Application/>
+    <Software/>
+    <Maintenance/>
+    <Branding/>
+    <UiUx/>
+    </div>
   )
 }
 
